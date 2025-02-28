59d7368c5811054529213c66804bc3388ffa0d90
tarasovcad <tarasovcad@gmail.com>
2025-02-27 20:30:32 -0600

    fix: fix autogrowing textareat and removed tabs animation

--- src/components/shadcn/textarea.tsx
+++ src/components/shadcn/textarea.tsx
@@ -2,18 +2,21 @@ import * as React from "react";
 
 import {cn} from "@/lib/utils";
 
-function Textarea({className, ...props}: React.ComponentProps<"textarea">) {
+const Textarea = React.forwardRef<
+  HTMLTextAreaElement,
+  React.ComponentProps<"textarea">
+>(({className, ...props}, ref) => {
   return (
     <textarea
-      data-slot="textarea"
       className={cn(
-        "flex w-full rounded-lg border border-input bg-background px-3 py-2 text-sm text-foreground shadow-sm shadow-black/5 transition-shadow placeholder:text-muted-foreground/70 focus-visible:border-ring focus-visible:outline-none focus-visible:ring-[3px] focus-visible:ring-ring/20 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive/60 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/25",
+        "flex min-h-[80px] w-full rounded-lg border border-input bg-background px-3 py-2 text-sm text-foreground shadow-sm shadow-black/5 transition-shadow placeholder:text-muted-foreground/70 focus-visible:border-ring focus-visible:outline-none focus-visible:ring-[3px] focus-visible:ring-ring/20 disabled:cursor-not-allowed disabled:opacity-50",
         className,
       )}
+      ref={ref}
       {...props}
     />
   );
-}
+});
 Textarea.displayName = "Textarea";
 
 export {Textarea};
--- src/components/ui/AutogrowingTextarea.tsx
+++ src/components/ui/AutogrowingTextarea.tsx
@@ -1,8 +1,53 @@
+// "use client";
+
+// import {Textarea} from "@/components/shadcn/textarea";
+// import {cn} from "@/lib/utils";
+// import {useRef, useEffect, useState} from "react";
+
+// export default function AutogrowingTextarea({
+//   id,
+//   placeholder,
+//   name,
+//   className,
+// }: {
+//   id: string;
+//   placeholder: string;
+//   name: string;
+//   className?: string;
+// }) {
+//   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+//   const [value, setValue] = useState("");
+
+//   useEffect(() => {
+//     if (textareaRef.current) {
+//       textareaRef.current.style.height = "auto";
+//       textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+//     }
+//   }, [value]);
+
+//   return (
+//     <div className="w-full">
+//       <Textarea
+//         id={id}
+//         name={name}
+//         ref={textareaRef}
+//         placeholder={placeholder || ""}
+//         className={cn(
+//           "resize-none overflow-hidden max-h-[150px] min-h-0",
+//           className,
+//         )}
+//         value={value}
+//         onChange={(e) => setValue(e.target.value)}
+//       />
+//     </div>
+//   );
+// }
+
 "use client";
 
-import {Textarea} from "@/components/shadcn/textarea";
+import {ChangeEvent, useRef} from "react";
+import {Textarea} from "../shadcn/textarea";
 import {cn} from "@/lib/utils";
-import {useRef, useEffect, useState} from "react";
 
 export default function AutogrowingTextarea({
   id,
@@ -15,30 +60,39 @@ export default function AutogrowingTextarea({
   name: string;
   className?: string;
 }) {
-  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
-  const [value, setValue] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const defaultRows = 1;
+  const maxRows = undefined;
 
-  useEffect(() => {
-    if (textareaRef.current) {
-      textareaRef.current.style.height = "auto";
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
-    }
-  }, [value]);
+  const handleInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const textarea = e.target;
+    textarea.style.height = "auto";
+
+    const style = window.getComputedStyle(textarea);
+    const borderHeight =
+      parseInt(style.borderTopWidth) + parseInt(style.borderBottomWidth);
+    const paddingHeight =
+      parseInt(style.paddingTop) + parseInt(style.paddingBottom);
+
+    const lineHeight = parseInt(style.lineHeight);
+    const maxHeight = maxRows
+      ? lineHeight * maxRows + borderHeight + paddingHeight
+      : Infinity;
+
+    const newHeight = Math.min(textarea.scrollHeight + borderHeight, maxHeight);
+
+    textarea.style.height = `${newHeight}px`;
+  };
 
   return (
-    <div className="w-full">
-      <Textarea
-        id={id}
-        name={name}
-        ref={textareaRef}
-        placeholder={placeholder || ""}
-        className={cn(
-          "resize-none overflow-hidden max-h-[150px] min-h-0",
-          className,
-        )}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-    </div>
+    <Textarea
+      id={id}
+      placeholder={placeholder}
+      ref={textareaRef}
+      name={name}
+      onChange={handleInput}
+      rows={defaultRows}
+      className={cn("min-h-[none] resize-none h-9", className)}
+    />
   );
 }
